8a74f2b5eac395234d97bb43f589b5afa48ecb68
David Searle <searled@shernet.sheridancollege.ca>
2025-02-21 07:07:37 -0500

    First button staged

---
+++ Assets/Scripts/Attack Spawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSpawner : MonoBehaviour
+{
+    float speed = 0.1f;
+    //public gameobjects to be referenced
+    public GameObject Fireball; 
+    //Spawned objects
+    GameObject fireball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (fireball != null) {
+           speed = .1f;
+       Vector3 fireballPosition = fireball.transform.position;
+        fireballPosition.x -= speed;
+        fireball.transform.position = fireballPosition;
+        if (fireballPosition.x <= -4.72f) {
+        Destroy(fireball);
+        }
+    }
+    }
+    public void spawnFireball() {
+        fireball = Instantiate(Fireball);
+    }
+
+}
+
